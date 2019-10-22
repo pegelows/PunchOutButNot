448c2f756c3a7e1f1feb1452e67066ed3b65c155
Sean Harvey <harveysr@gmatc.matc.edu>
2019-10-22 13:36:38 -0500

    Dodging V1

---
+++ Assets/Scripts/DodgeManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeManager : MonoBehaviour {
+    public bool snapToAngle = false;
+    public float maxLeanAngle = 45f; // Degrees
+
+    public float snapToDistance = 35f;
+    public float snapAtDistance = 15f;
+
+    public GameObject player;
+    public GameObject playerViewport;
+
+    private Vector3 startPosition;
+    private float playerHeight;
+
+    void Start() {
+        playerHeight = this.transform.size.y / 2f;
+        startPosition = this.transform.position - new Vector3(0, playerHeight, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float angle = playerViewport.transform.rotation.z;
+        float leanDirection = Mathf.Abs(angle) / angle;
+
+        if (snapToAngle) {
+            if (Mathf.Abs(angle) > snapAtDistance) {
+                angle = snapToDistance * leanDirection;
+            } else {
+                angle = 0f;
+            }
+        }
+
+        this.transform.localRotation = new Vector3(0, 0, angle);
+        this.transform.position = startPosition + (new Vector3(-leanDirection * Mathf.sin(angle), Mathf.cos(angle), 0f) * playerHeight);
+    }
+}
