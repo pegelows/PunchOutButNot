cff0bc93235f59eb5e90c7c602ad7b32aa1d8092
Andy <andypxiong08@gmail.com>
2019-12-03 14:08:09 -0600

    Main Menu Test 1
    Testing

---
+++ Assets/Scripts/PunchingBagOption.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchingBagOption : MonoBehaviour {
+    public void OnCollisionEnter(Collider other) {
+        if (other.GetComponent<GloveManager>() != null)
+        {
+            if (this.name == "Start")
+            {
+                GameObject.FindGameObjectWithTag("MainMenu").GetComponent<MainMenu>().PlayGame();
+            }
+            else if (this.name == "Quit")
+            {
+                GameObject.FindGameObjectWithTag("MainMenu").GetComponent<MainMenu>().QuitGame();
+            }
+        }
+    }
+}
